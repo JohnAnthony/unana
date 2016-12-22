55bd57403dc3662f4291aec0901390faf387fbd4
John Anthony <john@jo.hnanthony.com>
2016-12-22 23:34:18 +0000

    Basic framework to unana

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
+FLAGS=--std=c11 -Os
 .PHONY: clean
 
 all: mkdict unana
 
 mkdict: mkdict.c
-	cc mkdict.c -o mkdict
+	cc ${FLAGS} mkdict.c -o mkdict
 
 unana: unana.c
-	cc unana.c -o unana
+	cc ${FLAGS} unana.c -o unana
 
 clean:
 	rm mkdict unana
--- unana.c
+++ unana.c
@@ -1,4 +1,30 @@
+/*
+ * NAME
+ * 	   unana - Unanagram words using a dictionary, usually in O(1) time
+ *
+ * SYNOPSIS
+ *     unana ANAGRAM DICTIONARY
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// 26^6
+#define T_SZ 30891577
+
+typedef struct Result Result;
+struct Result {
+	char* val;
+	Result* next;
+};
+
 int
 main(void) {
-	return 0;
+	Result** table = calloc(T_SZ, sizeof(Result*));
+	if (!table) {
+		puts("Failed to allocate lookup table");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
