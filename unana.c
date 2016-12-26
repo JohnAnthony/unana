f56f8577dbc36f0590a666156adfe87e37a4bce8
John Anthony <john@jo.hnanthony.com>
2016-12-26 10:49:58 +0000

    Added a simple shell

--- unana.c
+++ unana.c
@@ -37,9 +37,9 @@ bool is_anagram(char* a, char* b) {
 
 int
 main(int argc, char** argv) {
-	if (argc != 3) {
+	if (argc != 2) {
 		puts("USAGE:");
-		puts("    unana ANAGRAM DICTIONARY");
+		puts("    unana DICTIONARY");
 		return EXIT_FAILURE;
 	}
 
@@ -49,9 +49,9 @@ main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
-	FILE* f = fopen(argv[2], "r");
+	FILE* f = fopen(argv[1], "r");
 	if (!f) {
-		printf("Unable to open dictionary: %s\n", argv[2]);
+		printf("Unable to open dictionary: %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -93,25 +93,38 @@ main(int argc, char** argv) {
 		ungetc(c, f);
 	}
 
-	// TODO: Make interactive, dictionary only loads once
 
-	char* target = strdup(argv[1]);
-	size_t keylen;
-	char* t_key = dict_key(target, strlen(target), &keylen); // Owned
-	size_t tt_key = table_key(t_key);
+	char buf[256];
+	while (1) {
+		printf(">> ");
+		if (fgets(buf, 256, stdin) == NULL)
+			break;
+		size_t len = strlen(buf);
 
-	printf("Using target '%s' with key '%s' (%u)\n", target, t_key, tt_key);
+		if (len == 0)
+			continue;
+		buf[len-1] = '\0';
 
-	Result *r = table[tt_key];
-	while (r) {
-		if (is_anagram(target, r->val))
-			printf("Found result: %s\n", r->val);
-		r = r->next;
+		if (strcmp(buf, "!exit") == 0)
+			break;
+
+		size_t keylen;
+		char* t_key = dict_key(buf, strlen(buf), &keylen); // Owned
+		size_t tt_key = table_key(t_key);
+
+		printf("Using target '%s' with key '%s' (%u)\n", buf, t_key, tt_key);
+
+		Result *r = table[tt_key];
+		while (r) {
+			if (is_anagram(buf, r->val))
+				printf("Found result: %s\n", r->val);
+			r = r->next;
+		}
+		free(t_key);
 	}
 
 CLEANUP:
 	puts("Freeing memory");
-	free(t_key);
 
 	for (i = 0; i < KEY_SPACE; i++) {
 		Result* r = table[i];
